6efe4b9e11d4ab91277ea12492405276b9d6f90e
hodaya123 <hodayak3@gmail.com>
2024-12-07 18:02:57 +0200

    add javascript

---
+++ index.js
@@ -0,0 +1,40 @@
+let numOfPeople;
+let bill;
+let tip;
+
+const onChangeNumOfPeople = () => {
+    const peopleElement = document.getElementById('people');
+    numOfPeople = peopleElement.value
+}
+const onChangeBill =()=>{
+    const billElement=document.getElementById('bill')
+    bill=billElement.value
+}
+const onChangeTip =()=>{
+    const priveElement=document.getElementById('price')
+    price=priveElement.value
+}
+const ChangeTotalAmount =()=>{
+    const amountPerPerson = bill / numOfPeople;
+    document.getElementById("price").innerHTML =amountPerPerson ;
+
+}
+// const ChangeTipPerPerson=()=>{
+//     const totalAmount = bill ;
+//     const amountPerPerson = totalAmount / numOfPeople;
+
+
+// }
+const changeTipPerPerson = (button) => {
+    const tipPercent = parseInt(button.getAttribute('data-tip'));
+    const totalAmount = bill ;
+    const amountPerPerson = totalAmount / numOfPeople;
+    const totalTip= amountPerPerson * tipPercent*0.01;
+    document.getElementById("tipPerson").innerHTML =totalTip ;
+
+} 
+
+const changeTip = (element) => {
+    tipPercent = element.value;
+    changeTipPerPerson()
+}
