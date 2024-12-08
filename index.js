c27a4dc163ceaa24c9926b596cb75d22b9f455c2
ShayMashiah <Shay2908@gmail.com>
2024-12-08 15:31:05 +0200

    Updated code after CR

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 let totalAmount;
 
 
-const calculateTip = (x) => {
-    const amount = document.getElementById('amountOfBill').value;
+const calculateTip = (tipPercentage) => {
+    const amount = document.getElementById('bill-input').value;
     if (amount === '') {
       alert('Please enter the amount of bill');
       return;
     }
     else{
-         totalAmount = amount*x;
+         totalAmount = amount*tipPercentage;
         }
     document.getElementById('tip-amount').innerHTML = (totalAmount - amount).toFixed(2) + "$";
   };
   
   const updatePeople = () => {
-    const numberOfPeople = document.getElementById('numOfPeople').value;
-    const amount = document.getElementById('amountOfBill').value;
+    const numberOfPeople = document.getElementById('num-of-people-input').value;
+    const amount = document.getElementById('bill-input').value;
     if (numberOfPeople == 0 || amount === '') {
       alert('Please enter the number of people');
       return;
@@ -25,8 +25,8 @@ const calculateTip = (x) => {
   };
 
     const reset = () => {
-        document.getElementById('amountOfBill').value = '';
-        document.getElementById('numOfPeople').value = '';
+        document.getElementById('bill-input').value = '';
+        document.getElementById('num-of-people-input').value = '';
         document.getElementById('tip-amount').innerHTML = '0.00$';
         document.getElementById('total-amount').innerHTML = '0.00$';
     }
