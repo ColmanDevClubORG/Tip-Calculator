23f1c68b187654a65bcd32a7ece9be416a5f1cd6
emilyluskin <emilyluskinn@gmail.com>
2024-12-08 11:53:50 +0200

    updated html, css and js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
 const buttonsCollection = document.getElementsByClassName("tip-percent-btn");
-const printry= ()=>{
-    for (let i = 0; i < collection.length; i++) {
-        console.log(collection[i]);
-      }
-}
